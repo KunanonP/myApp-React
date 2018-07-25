209fd3dce898c8f8d4e103295324e79c1b83bd02
Brightbite <zindybiteme@gmail.com>
2018-07-26 03:39:33 +1000

    button test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+  super(props);
+  this.handleClick = this.handleClick.bind(this);
   }
-}
+  handleClick() {
+    console.log('Click happened');
+
+  }
+  render() {
+    return <button onClick={this.handleClick}>Click Me</button>;
+  }
+  }
 
 export default App;
